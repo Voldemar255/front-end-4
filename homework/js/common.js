69d4bd5533be41f8e14113bb13bc757fe86bcc4b
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2017-11-09 22:10:22 +0200

    function

---
+++ homework/js/common.js
@@ -0,0 +1,12 @@
+function Sered(a,b) {
+    s=(a+b)/2;
+    console.log('Середнє арифметичне двох чисел=',s);
+}
+
+function Minimal(a,b) {
+    if (a < b) {
+        console.log('Менше число=', a);
+    }
+    else
+        console.log('Менше число=', b);
+}
