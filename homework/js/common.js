9a3e0353ecf321d255f3ff05b7a0d6c565ebfc17
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2017-11-21 23:00:50 +0200

    change func

--- homework/js/common.js
+++ homework/js/common.js
@@ -1,34 +1,27 @@
 // 1.1 Функція перевірки масива на наявність елементів//
-
-var length=[45, 109, 32, 39, 1, 0];
-function isEmpty(length) {
-    for
-    (var element in length) {
+var length = [45, 109, 32, 39, 1, 0];
+function isEmpty(array) {
+    for (var element in array) {
         return true;
     }
     return false;
 }
-
+console.log('-----', isEmpty(length));
 
 // 1.2 Функція суми елементів масива//
-
-function sumData(length) {
-    if (isEmpty(length)) {
-        var summa=0;
-        for
-            (var element in length) {
-            summa += length[element];
+function sumData(array) {
+    if (isEmpty(array)) {
+        var summa = 0;
+        for (var element in array) {
+            summa += array[element];
         }
         return summa;}
-
 }
+console.log('-----', sumData(length));
 
-var codeObj = [length];
-codeObj [23] = 95;
-function sum(codeObj) {
-    var summa=0;
-    for (var element in length){
-        summa += length[element];
-    }
-    return summa;
-}
+// 1.3 Функція суми елементів масива//
+var codeObj = {
+    'length' : length
+};
+codeObj['length'][23] = 95;
+console.log('-----', sumData(codeObj['length']));
