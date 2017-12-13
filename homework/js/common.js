9001a31e7af7cc753c9848706a3fcade7ee474b3
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2017-12-13 14:57:52 +0200

    create custom-slider

--- homework/js/common.js
+++ homework/js/common.js
@@ -6,3 +6,26 @@ $(document).ready(function(){
         autoplaySpeed: 4000,
 });
 });
+
+
+
+$(document).ready(function(){
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.user-slide'
+    });
+    $('.user-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+});
