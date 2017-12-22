07633b9c2a652c0549a77cbcc977b0635ace84d4
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2017-12-22 23:06:58 +0200

    validation

--- homework/js/common.js
+++ homework/js/common.js
@@ -1,26 +1,31 @@
 "use strict";
+$( "#toggle-button" ).on( "mousemove", function(event) {
+    event.preventDefault(),
+    $('.some-form').toggle();
+});
+
+$( "#toggle-button" ).on( "mouseleave", function() {
+    $('.some-form').hidden;
+});
 
 $(document).ready(function () {
     $('.main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 4000,
     });
-    // $('.slider-for').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     fade: true,
-    //     asNavFor: '.user-slider'
-    // });
-    // $('.user-slide').slick({
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     asNavFor: '.slider-for',
-    //     dots: true,
-    //     centerMode: true,
-    //     focusOnSelect: true
-    //
-    // });
+
+    $('.main-thumb-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.main-slider',
+        autoplaySpeed: 4000,
+        centerMode: true,
+        focusOnSelect: true,
+    });
+
+
 
 });
 function initMap() {
@@ -34,3 +39,40 @@ function initMap() {
         map: map
     });
 }
+
+$('name-form').validate({
+    rules: {
+        name: {
+            required: true,
+            minlength: 1,
+        }
+    }
+});
+
+
+$('email-form').validate({
+    rules: {
+        email: {
+            required: true,
+            email: true
+}
+    }
+});
+
+
+$('message-form').validate({
+    rules: {
+        message: {
+
+        }
+    }
+});
+
+$('your-email').validate({
+    rules: {
+        name: {
+            required: true
+}
+}
+});
+
