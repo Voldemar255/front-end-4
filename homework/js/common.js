8851181b92a36db269098164491f628ff4cc1ac7
Volodymyr Vinnichuk <vinnichuk.vladimir@gmail.com>
2017-12-28 01:04:08 +0200

    validation crash

--- homework/js/common.js
+++ homework/js/common.js
@@ -1,10 +1,10 @@
 "use strict";
-$( "#toggle-button" ).on( "mousemove", function(event) {
+$("#toggle-button").on("mousemove", function (event) {
     event.preventDefault(),
-    $('.some-form').toggle();
+        $('.some-form').toggle();
 });
 
-$( "#toggle-button" ).on( "mouseleave", function() {
+$("#toggle-button").on("mouseleave", function () {
     $('.some-form').hidden;
 });
 
@@ -15,7 +15,7 @@ $(document).ready(function () {
         slidesToScroll: 1,
         autoplay: true,
         asNavFor: '.main-thumb-slider',
-        autoplaySpeed: 4000
+        autoplaySpeed: 4000,
     });
 
     $('.main-thumb-slider').slick({
@@ -30,8 +30,8 @@ $(document).ready(function () {
     });
 
 
-
 });
+
 function initMap() {
     var khmelnitsky = {lat: 49.422983, lng: 26.987133};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -44,39 +44,35 @@ function initMap() {
     });
 }
 
-$('name-form').validate({
-    rules: {
-        name: {
-            required: true,
-            minlength: 1,
+
+
+$(function () {
+    $('name-form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 1
+            }
         }
-    }
-});
+    });
 
-
-$('email-form').validate({
-    rules: {
-        email: {
-            required: true,
-            email: true
-}
-    }
-});
-
-
-$('message-form').validate({
-    rules: {
-        message: {
-
+    $('email-form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
         }
-    }
-});
+    });
 
-$('your-email').validate({
-    rules: {
-        name: {
-            required: true
-}
-}
-});
+    $('your-email').validate({
+        rules: {
+            name: {
+                required: true
+            }
+        }
+    });
+
+})
+
 
